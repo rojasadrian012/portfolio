0a55b16de051965d6a9a6ed5265b7317adaf3ae1
rojasadrian012 <rojasadrian012@gmail.com>
2025-08-21 17:52:00 -0300

    Improvement in the LanguagePicker component to ensure the language type and a link to the Moura company is added in the translations. The translation function is adjusted to properly handle data types

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -7,7 +7,7 @@ export function getLangFromUrl(url: URL) {
 }
 
 export function useTranslations(lang: keyof typeof ui) {
-    return function t(key: keyof typeof ui[typeof defaultLang]) {
-        return ui[lang][key] || ui[defaultLang][key];
+    return function t(key: keyof typeof ui[typeof defaultLang]): string {
+        return (ui[lang]?.[key] || ui[defaultLang][key]) as string;
     }
 }
